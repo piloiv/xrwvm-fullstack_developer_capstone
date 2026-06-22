bf7011b48a45c16466fb31bb3b7ea551dfb13b83
piloiv <piloiv@gmail.com>
2026-06-22 08:36:10 -0400

    Completed mod 2 exercises

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,17 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://theianext-1-labs-prod-misc-tools-us-east-0.labs.cognitiveclass.ai:8000']
-CSRF_TRUSTED_ORIGINS = ['https://theianext-1-labs-prod-misc-tools-us-east-0.labs.cognitiveclass.ai:8000']
+# ALLOWED_HOSTS = ['localhost','https://theianext-1-labs-prod-misc-tools-us-east-0.labs.cognitiveclass.ai:8000']
+# CSRF_TRUSTED_ORIGINS = ['https://theianext-1-labs-prod-misc-tools-us-east-0.labs.cognitiveclass.ai:8000']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "piloiv-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://piloiv-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,7 +70,13 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'frontend/static')],
+        # 'DIRS': [os.path.join(BASE_DIR,'frontend/static')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
+        ],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,5 +149,10 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'frontend/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'frontend/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static'),
+	os.path.join(BASE_DIR, 'frontend/build'),
+	os.path.join(BASE_DIR, 'frontend/build/static'),
+]
 
